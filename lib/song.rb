7ce94c0b049ac1a5a34a49690ede2d22ba27796d
Heather Kirk <hskirk@gmail.com>
2019-02-12 22:34:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,13 +35,18 @@ class Song
   end 
   
   def self.find_or_create_by_name(name)
+<<<<<<< HEAD
     self.find_by_name(name) || self.create_by_name(name)
+=======
+    self.find_by_name || self.create_by_name
+>>>>>>> fb997fcd3a9867828465bdc5f0cb438c942b1080
   end
 
   def self.alphabetical
   @@all.sort_by {|song| song.name}
   end
   
+<<<<<<< HEAD
   def self.new_from_filename(filename)
   
     data = filename.gsub(".mp3", "").split(" - ")
@@ -59,6 +64,18 @@ class Song
     song.artist_name = data[0]
     data.join
     song 
+=======
+  def self.new_from_filename(name)
+    new_song = self.new
+    new_song.name = name 
+   
+  end 
+  
+  def self.create_from_filename
+    data = filename 
+    filename = self.new 
+    filename.split(" - ")
+>>>>>>> fb997fcd3a9867828465bdc5f0cb438c942b1080
   end
   
   def self.destroy_all
